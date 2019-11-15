5cbdc92e8db160da6c770f5fe9a172589e02628f
sudeepcv <sudeep.cv@piserve.com>
2019-11-15 12:05:57 +0530

    init

---
+++ src/main/java/com/basicsinjava/springbootpostgres/SpringbootPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.basicsinjava.springbootpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basicsinjava/springbootpostgres/SpringbootPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basicsinjava.springbootpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
