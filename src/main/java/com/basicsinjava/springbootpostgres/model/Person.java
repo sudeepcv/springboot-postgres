62702765933253ebe2c5ec5ed68c30276b7121b5
Sudeep-cv <basicsinjava@gmail.com>
2019-11-17 09:46:09 +0000

    config update

--- src/main/java/com/basicsinjava/springbootpostgres/model/Person.java
+++ src/main/java/com/basicsinjava/springbootpostgres/model/Person.java
@@ -1,14 +1,14 @@
-// package com.basicsinjava.springbootpostgres.model;
+package com.basicsinjava.springbootpostgres.model;
 
-// import javax.persistence.Entity;
-// import javax.persistence.GeneratedValue;
-// import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
-// @Entity
-// public class Person{
-//     @Id
-//     @GeneratedValue
-//     Long id;
-//     String name;
+@Entity
+public class Person{
+    @Id
+    @GeneratedValue
+    Long id;
+    String name;
 
-// }
+}
